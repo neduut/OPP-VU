77ae23b80d4ec7793d998da7353e449df7b037f0
Neda Davidavičiūtė <neda.davidaviciute@mif.stud.vu.lt>
2025-02-25 23:48:08 +0200

    DYNAMIC C ARRAY DONEE.h

--- mixed.h
+++ mixed.h
@@ -5,79 +5,15 @@
 #include "utils.h"
 
 struct StudentMixed {
-    string firstName;   
-    string lastName;    
-    int *marks = nullptr;
-    int marksCount = 0;  
-    int examMark = 0;      
+    string firstName, lastName; 
+    int *marks;
+    int marksCount;
+    int examMark;    
 };
 
-//failed dynamic c array implementation
-/*struct StudentMixed {
-    string firstName;
-    string lastName;
-    int* marks;
-    int marksCount;
-    int marksCapacity;
-    int examMark;
-
-    StudentMixed() {
-        marks = new int[1]();
-        marksCount = 0;
-        marksCapacity = 1;
-        examMark = 0;
-    }
-
-    ~StudentMixed() {
-        delete[] marks;
-    }
-};*/
-
-/*
-//second try
-struct StudentMixed {
-    string firstName;
-    string lastName;
-    int* marks;
-    int marksCount;
-    int marksCapacity;
-    int examMark;
-
-    StudentMixed() {
-        marks = new int[1]();  
-        marksCount = 0;
-        marksCapacity = 1;
-        examMark = 0;
-    }
-
-    void addMark(int mark) {
-        if (marksCount == marksCapacity) {
-            marksCapacity *= 2;  
-            int* newMarks = new int[marksCapacity];
-            
-            for (int i = 0; i < marksCount; i++) {
-                newMarks[i] = marks[i];
-            }
-
-            delete[] marks;
-            marks = newMarks;
-        }
-
-        marks[marksCount] = mark;
-        marksCount++;  
-    }
-
-    ~StudentMixed() {
-        delete[] marks; 
-    }
-};*/
-
-
-void readInput(vector<StudentMixed>& students, string choice);
+void readInput(vector<StudentMixed>& students, string menuChoice);
 double averageFinalMark(const StudentMixed& student);
 double medianFinalMark(const StudentMixed& student);
-void output(vector<StudentMixed>& students);
-// failed 3rd time function void readInput(vector<StudentMixed>& students);
+void output(vector<StudentMixed>& students, string finalType);
 
 #endif 
-
