a6bd9d7d65b5ce97f2a389329dd5ab1c33da8320
Neda Davidavičiūtė <neda.davidaviciute@mif.stud.vu.lt>
2025-02-25 23:17:11 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,11 +1,16 @@
-#ifndef VESTOR_H
-#define VECTOR_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include "mylib.h"
 #include "constants.h"
 #include "utils.h"
 
-#include "vector.h"
-#include "mixed.h"
+#define USE_VECTOR
 
-#endif  
+#ifdef USE_VECTOR
+    #include "vector.h"
+#else
+    #include "mixed.h"
+#endif
+
+#endif // MAIN_H
